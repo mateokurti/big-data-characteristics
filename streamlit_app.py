ab2dbd12a16c1054ff1412cb6882a22c12063d5a
Mateo Kurti <contact@mateokurti.com>
2025-02-15 23:09:21 +0100

    Add basic streamlit structure

---
+++ streamlit_app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv("patients_data.csv", sep=";")  # Ensure correct delimiter
+    return df
+
+
+# Set page configuration
+st.set_page_config(page_title="Big Data Characteristics", page_icon="📊", layout="wide")
+
+if "data" not in st.session_state:
+    st.session_state.data = load_data()
+
+if "team_members" not in st.session_state:
+    st.session_state.team_members = [
+        "Mateo Kurti",
+        "Dea Peka",
+        "Klea Halili",
+        "Andri Peti",
+    ]
